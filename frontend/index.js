d708b36f8dc8edc51624afbca95b203e9ddc0101
Ankit Poonia <apoonia287@gmail.com>
2023-12-06 11:57:52 +0530

    fronted Almost completed

---
+++ frontend/index.js
@@ -0,0 +1,24 @@
+
+let slideIndex = 0;
+const slides = document.querySelectorAll('.custom-carousel-item');
+
+function showSlide(n) {
+    slides.forEach(slide => slide.style.display = 'none');
+    if (n < 0) {
+        slideIndex = slides.length - 1;
+    } else if (n >= slides.length) {
+        slideIndex = 0;
+    }
+    slides[slideIndex].style.display = 'block';
+}
+
+function nextSlide() {
+    showSlide(slideIndex += 1);
+}
+
+function prevSlide() {
+    showSlide(slideIndex -= 1);
+}
+
+// Optional: Auto slide change every 2 seconds
+setInterval(nextSlide, 2000);
